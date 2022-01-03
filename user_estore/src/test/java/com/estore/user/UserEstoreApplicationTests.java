4fa494eb135c2764434791a6191b359023cf99b7
Saloni Sharma <salonisharma0816@gmail.com>
2022-01-03 12:34:15 +0530

    initial

---
+++ user_estore/src/test/java/com/estore/user/UserEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estore.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
